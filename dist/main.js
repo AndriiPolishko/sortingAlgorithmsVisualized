cfa78386741a3a16a542c5a4916e6778a6fcde52
Andrii Polishko <34601113+AndriiPolishko@users.noreply.github.com>
2022-06-29 20:25:05 +0300

    Revert "Manage speed"

--- dist/main.js
+++
@@ -1,9 +0,0 @@
-import { generateElements } from './generateElements.js';
-import { bubbleSort } from './bubbleSort.js';
-const main = document.querySelector('main');
-const bubbleSortButton = document.querySelector('#js_bubbleSort');
-bubbleSortButton === null || bubbleSortButton === void 0 ? void 0 : bubbleSortButton.addEventListener('click', bubbleSort);
-const reset = document.querySelector('#js_reset');
-window.addEventListener('load', generateElements);
-reset === null || reset === void 0 ? void 0 : reset.addEventListener('click', generateElements);
-export { main };
--- readme.md
+++
@@ -1,5 +0,0 @@
-# Visualization of sorting algorithms
-
-This side project focused on visualizing most common sorting algorithms.
-
-**_here will be project updated during development_**
